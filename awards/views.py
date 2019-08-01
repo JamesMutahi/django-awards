ecc4db9b1399a929b4bb1440bbf0d7dbaa5f7188
JamesMutahi <mutahijames0@gmail.com>
2019-08-01 13:45:05 +0300

    Add views for profile page

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', locals())
+
+
+def profile(request):
+    return render(request, 'profile.html', locals())
+
+
