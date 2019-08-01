f7a71a4ce6d08e460094eabda1386d7d9e296344
JamesMutahi <mutahijames0@gmail.com>
2019-08-01 13:42:58 +0300

    Add urls for profile page

--- awards/urls.py
+++ awards/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^accounts/profile/$', views.profile, name='profile'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
