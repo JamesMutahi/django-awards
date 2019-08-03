56ad5c04f4445a3827b2232fb9d21d6aaf3e3ecd
JamesMutahi <mutahijames0@gmail.com>
2019-08-03 21:10:59 +0300

    Update

--- awards/forms.py
+++ awards/forms.py
@@ -9,3 +9,12 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('image', 'bio')
+
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'post_date']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
