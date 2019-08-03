cdafa763b54db3fe879e89c4e38b4ed0d1cb8fe6
JamesMutahi <mutahijames0@gmail.com>
2019-08-03 21:09:58 +0300

    Add view to display projects and show project's author's page

--- awards/views.py
+++ awards/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Profile
-from .forms import ProfileForm
+from .models import Profile, Post
+from .forms import ProfileForm, NewPostForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
@@ -31,3 +33,33 @@ def edit(request):
     else:
         new_profile = ProfileForm(instance=request.user.profile)
     return render(request, 'edit.html', locals())
+
+
+@login_required(login_url='/accounts/login/')
+def project(request):
+    posts = Post.objects.all().order_by('-post_date')
+    return render(request, 'projects.html', locals())
+
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect('project')
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form": form})
+
+
+@login_required(login_url='/accounts/login/')
+def user(request, user_id):
+    user_object = get_object_or_404(User, pk=user_id)
+    if request.user == user_object:
+        return redirect('profile')
+    user_images = user_object.posts.all()
+    return render(request, 'user.html', locals())
