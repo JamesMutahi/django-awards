9b140d8975e47d75008ba938354b06a1a2d8d954
JamesMutahi <mutahijames0@gmail.com>
2019-08-03 21:10:48 +0300

    add urls

--- awards/urls.py
+++ awards/urls.py
@@ -5,8 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^projects/$', views.project, name='project'),
+    url(r'^new/post$', views.new_post, name='new_post'),
     url(r'^accounts/profile/$', views.profile, name='profile'),
     url(r'^accounts/profile/edit/$', views.edit, name='edit'),
+    url(r'^user/(?P<user_id>\d+)$', views.user, name='aboutuser'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
