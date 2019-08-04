c1de6f339abb961ced2f09187895034861895e82
JamesMutahi <mutahijames0@gmail.com>
2019-08-04 20:26:42 +0300

    Add url for single page

--- awards/urls.py
+++ awards/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     url(r'^accounts/profile/$', views.profile, name='profile'),
     url(r'^accounts/profile/edit/$', views.edit, name='edit'),
     url(r'^user/(?P<user_id>\d+)$', views.user, name='aboutuser'),
+    url(r'^project/(\d+)$', views.single_project, name='project'),
+    url(r'^rating/(\d+)$', views.review_rating, name="review"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
